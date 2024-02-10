42a2e1bfba104457e1c41bca8bc8b1fd6e1bfb23
Roberto <roberto.quadraccia90@gmail.com>
2024-02-10 14:33:39 +0100

    Filenames explained, about folder with a page.js file inside created as additional route within the app folder

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+// This is a regular React component but treated in a special way by NextJS.
+// It is treated as a server component and executed on the server,
+// and it's then the returned JSX code that's sent over the wire to the browser to be rendered
+// as HTML, and that's why we can see what we see here, thanks to this component.
+
+// IMPORTANT: The file name (page) matters because it tells NextJS that we want to have this as a page.
+
 export default function Home() {
   return (
     <main>
